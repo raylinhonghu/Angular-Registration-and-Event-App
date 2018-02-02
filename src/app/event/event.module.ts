e89ddb1a567d41bf71ab03a201052f18b5a5a262
raylinhonghu <raylinhonghu@gmail.com>
2018-02-02 14:29:49 -0800

    events, registration, sub events, hook up to form.io

---
+++ src/app/event/event.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormioResource, FormioResourceRoutes, FormioResourceConfig, FormioResourceService } from 'angular-formio/resource';
+import { ParticipantModule } from './participant/participant.module';
+import { EventResourceComponent } from './event-resource/event-resource.component';
+
+const eventResourceRoutes: Routes = FormioResourceRoutes({
+  resource: EventResourceComponent
+});
+
+eventResourceRoutes[2].children.push({
+  path: 'participant',
+  loadChildren: () => ParticipantModule
+});
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormioResource,
+    RouterModule.forChild(eventResourceRoutes)
+  ],
+  declarations: [EventResourceComponent],
+  FormioResourceService,
+  providers: [
+    FormioResourceService,
+    {provide: FormioResourceConfig, useValue: {
+      name: 'event',
+      form: 'event'
+    }}
+  ]
+})
+export class EventModule { }
